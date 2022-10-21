46deee6b8e2af642887257a65771d8a666e96fe3
Dmitry <dmitrychinyaev93@yandex.ru>
2022-10-21 20:24:07 +0300

    Stub and tests for add1

--- src/main/java/Phonebook.java
+++ src/main/java/Phonebook.java
@@ -1,2 +1,6 @@
 public class Phonebook {
+
+    public boolean add(){
+        return false;
+    }
 }
--- src/test/java/PhonebookTest.java
+++ src/test/java/PhonebookTest.java
@@ -1,2 +1,43 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhonebookTest {
+    @BeforeEach
+    public void init() {
+        System.out.println("Test started");
+        Object sut = null;
+    }
+
+    @BeforeAll
+    public static void started() {
+        System.out.println("Tests started");
+    }
+
+    @AfterEach
+    public void finished() {
+        System.out.println("Test completed");
+        //sut = null;
+    }
+
+    @AfterAll
+    public static void finishedAll() {
+        System.out.println("Tests completed");
+    }
+
+    @Test
+    public void add() {
+        String name1 = "Ivan";
+        String number1 = "89991112233";
+
+        int expected = 1;
+
+        int result = sut.add(name1,number1);
+
+        assertEquals(expected,result);
+    }
+
+    
 }
+
+
